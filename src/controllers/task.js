4a7b7325da2d3a50ed69137fddad0dc114ab9e16
idris agboola <agboolaisholaidreez@gmail.com>
2021-03-12 15:15:13 +0100

    update

--- src/controllers/task.js
+++ src/controllers/task.js
@@ -2,7 +2,24 @@ const Task = require("../models/task");
 
 exports.getTasks = async (req, res) => {
   try {
-    res.json({ msg: "get task" });
+    let main = {};
+    let sort = {};
+
+    if (req.query.complete) {
+      main = { done: req.query.complete };
+    }
+
+    if (req.query.sortBy) {
+      let part = req.query.sortBy.split(":");
+      sort[part[0]] = part[1] === "desc" ? -1 : 1;
+    }
+    const user = await Task.find(main, null, {
+      limit: parseInt(req.query.limit),
+      skip: parseInt(req.query.skip),
+      sort,
+    });
+
+    res.json(user);
   } catch (error) {
     res.status(400).json({ msg: error.message });
   }
--- src/models/task.js
+++ src/models/task.js
@@ -1,9 +1,12 @@
 const { model, Schema } = require("mongoose");
-const schema = new Schema({
-  description: { type: String, required: true },
-  done: { type: Boolean, default: false },
-  userID: { type: Schema.Types.ObjectId, ref: "User", required: true },
-});
+const schema = new Schema(
+  {
+    description: { type: String, required: true },
+    done: { type: Boolean, default: false },
+    userID: { type: Schema.Types.ObjectId, ref: "User", required: true },
+  },
+  { timestamps: true }
+);
 
 const task = model("Task", schema);
 
